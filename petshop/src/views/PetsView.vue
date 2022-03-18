e2b45461d00383e494481977cd8e24cdb4669987
StivenArboleda <stiven2201@gmail.com>
2022-03-17 19:49:50 -0500

    Use 'dogs api' to display images of dogs in a grid

---
+++ petshop/src/views/PetsView.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container fluid grid-list-md>
+        <v-layout wrap>
+            <v-flex xs12 sm4 md3 v-for="pet in dogs" :key="pet.breed">
+                <v-card>
+                    <v-img :src="pet.img" height="250px">
+                        
+                    </v-img>
+                    <v-card-title>
+                        <div>
+                            <h3>{{pet.name}}</h3>
+                            <p>{{pet.breed}}</p>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+
+    </v-container>
+</template>
+
+<script>
+import Dogs from "../data/dogs";
+
+export default {
+  data: () => ({
+    dogs: Dogs,
+  }),
+};
+</script>
